5c8bb48a7a7d4482998e7d270ee95e5362470ab2
Vivek <vvivksingh@gmail.com>
2021-12-22 08:25:57 +0530

    Use Case 1

---
+++ GamblingSimulator.java
@@ -0,0 +1,6 @@
+public class GamblingSimulator{
+	public static void main(String[] args){
+		public static final int STARTING_BET_STAKE = 100;
+		public static final int MINIMUM_BET_PER_GAME = 1;
+	}
+}
